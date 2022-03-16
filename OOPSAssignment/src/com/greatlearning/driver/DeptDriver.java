580b332e810b7bd4db2e95b50c24737671326b02
Aravindsmr <99193020+Aravindsmr@users.noreply.github.com>
2022-03-16 23:04:30 +0530

    adding comments for the necessary actions

--- OOPSAssignment/src/com/greatlearning/driver/DeptDriver.java
+++ OOPSAssignment/src/com/greatlearning/driver/DeptDriver.java
@@ -7,22 +7,26 @@ import com.greatlearning.departments.TechDepartment;
 public class DeptDriver {
 
     public static void main(String[] args) {
-
+        
+        // creating objects for the departments
         HrDepartment hrDept = new HrDepartment();
         AdminDepartment adminDept = new AdminDepartment();
         TechDepartment techDept = new TechDepartment();
 
+        // printing admin department methods
         System.out.println("\n Welcome to" + adminDept.departmentName());
         System.out.println(adminDept.getTodaysWork());
         System.out.println(adminDept.getWorkDeadline());
         System.out.println(adminDept.isTodayAHoliday());
 
+        // printing HR department methods
         System.out.println("\n\n Welcome to" + hrDept.departmentName());
         System.out.println(hrDept.doActivity());
         System.out.println(hrDept.getTodaysWork());
         System.out.println(hrDept.getWorkDeadline());
         System.out.println(hrDept.isTodayAHoliday());
 
+        // printing tech department methods
         System.out.println("\n\n Welcome to" + techDept.departmentName());
         System.out.println(techDept.getTodaysWork());
         System.out.println(techDept.getWorkDeadline());
